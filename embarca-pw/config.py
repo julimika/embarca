3e4fd85be2bdc66a5233d8cdadc31e402e437126
Mika <julimika@proton.me>
2025-07-02 16:30:22 -0300

    remove config file

--- embarca-pw/config.py
+++
@@ -1,8 +0,0 @@
-wrong_email = 'abacaxi@gmail.com'
-wrong_password = 'abacaxi123'
-
-right_email = 'daxexe6870@jazipo.com'
-right_password = '123@Daxexe'
-origem = 'Curitiba'
-
-date = '20/05/2025'
--- embarca-pw/test_embarca.py
+++ embarca-pw/test_embarca.py
@@ -11,36 +11,42 @@ import pytest
 
 class TestLogin:
 
-    def test_incorrect_login(self, page: Page):
-        page.goto("https://www.embarca.ai")
-        locator = page.get_by_text("Entrar", exact=True)
-        locator.hover()
-        locator.click()
-        page.get_by_placeholder("Insira aqui seu e-mail, CPF ou CNPJ...").first.fill(config.wrong_email)
-        page.get_by_placeholder("Insira sua senha...").first.fill(config.wrong_password)
-        page.locator("button.cursor-pointer:nth-child(3)").click()
-        time.sleep(2)
+    # def test_incorrect_login(self, page: Page):
+    #     page.goto("https://www.embarca.ai")
+    #     locator = page.get_by_text("Entrar", exact=True)
+    #     locator.hover()
+    #     locator.click()
+    #     page.get_by_placeholder("Insira aqui seu e-mail, CPF ou CNPJ...").first.fill(config.wrong_email)
+    #     page.get_by_placeholder("Insira sua senha...").first.fill(config.wrong_password)
+    #     page.locator("button.cursor-pointer:nth-child(3)").click()
+    #     time.sleep(2)
     
 
-    def test_correct_login(self, page: Page):
-        page.goto("https://www.embarca.ai")
-        locator = page.get_by_text("Entrar", exact=True)
-        locator.hover()
-        locator.click()
-        page.get_by_placeholder("Insira aqui seu e-mail, CPF ou CNPJ...").first.fill(config.right_email)
-        page.get_by_placeholder("Insira sua senha...").first.fill(config.right_password)
-        page.locator("button.cursor-pointer:nth-child(3)").click()
-        time.sleep(2)
+    # def test_correct_login(self, page: Page):
+    #     page.goto("https://www.embarca.ai")
+    #     locator = page.get_by_text("Entrar", exact=True)
+    #     locator.hover()
+    #     locator.click()
+    #     page.get_by_placeholder("Insira aqui seu e-mail, CPF ou CNPJ...").first.fill(config.right_email)
+    #     page.get_by_placeholder("Insira sua senha...").first.fill(config.right_password)
+    #     page.locator("button.cursor-pointer:nth-child(3)").click()
+    #     time.sleep(2)
 
     def test_search_ticket(self, page: Page):
         page.goto("https://www.embarca.ai")
-        locator = page.get_by_text("Entrar", exact=True)
-        locator.hover()
-        locator.click()
-        page.get_by_placeholder("Insira aqui seu e-mail, CPF ou CNPJ...").first.fill(config.right_email)
-        page.get_by_placeholder("Insira sua senha...").first.fill(config.right_password)
-        page.locator("button.cursor-pointer:nth-child(3)").click()
-        time.sleep(2)
+        # locator = page.get_by_text("Entrar", exact=True)
+        # locator.hover()
+        # locator.click()
+        # page.get_by_placeholder("Insira aqui seu e-mail, CPF ou CNPJ...").first.fill(config.right_email)
+        # page.get_by_placeholder("Insira sua senha...").first.fill(config.right_password)
+        # page.locator("button.cursor-pointer:nth-child(3)").click()
         page.locator('div.border-e:nth-child(1) > div:nth-child(2) > input:nth-child(2)').fill(config.origem)
+        time.sleep(2)
         page.locator('li.px-5:nth-child(1)').click() #select first option in list
         time.sleep(2)
+        page.locator('div.border-e:nth-child(2) > div:nth-child(2) > input:nth-child(2)').fill(config.destino)
+        time.sleep(2)
+        page.locator('li.px-5:nth-child(1)').click() 
+        page.locator("div.border-e:nth-child(3) > div:nth-child(2) > input:nth-child(2)").click() #ida
+        page.get_by_text('12').click()
+        time.sleep(3)
